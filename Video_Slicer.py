8fbbe24f27edd8a5aa3e77adcff6ba0ed76f0c1a
mmadaio <mmadaio@cs.cmu.edu>
2018-01-24 13:10:56 -0500

    Update Video_Slicer.py

--- Video_Slicer.py
+++ Video_Slicer.py
@@ -3,21 +3,20 @@ import time
 
 def write_slice_bat_file():
 
-    filepath = "C:\Users\\articulab.rae\Desktop" ## Change to your local filepath where the Batch# folder is
-    input_batch_folder = filepath + "\Batch5\\"  ## Change to be whichever batch you're on
-    dyad = ["D15_S2", "D16_S1", "D17_S2"]  ## Change these to your video files
-    length_of_video_in_slices = [112, 106, 115]   ## Change these to be the length of your video, in 30-second slices
+    filepath = "C:\Users\\articulab.rae\Desktop\RAPT_WoZ_Videos\\" ## Change to your local filepath where the video file folder is
+    participant = ["P1", "P2", "P3"]  ## Change these to the start of the video file names
+    length_of_video_in_slices = [112, 106, 115]   ## Change these to be the length of each video, in 30-second slices. Be sure to put these in the order of the participant numbers above
 
     filename = "video_slicer.bat"   ## This is the name of the file created by running this script
     text = []
 
-    for k in range(len(dyad)):
+    for k in range(len(participant)):
         print k
-        j = dyad[k]
+        j = participant[k]
         print j
-        input_file = j + "_Both.mp4"   ## Change this to the end of your video files
-        output_slice_folder = input_batch_folder + "Slices" + "\\" + j + "\\"
-
+        input_file = j + ".mp4"   ## Change this to the end of your video files (should be just .mp4)
+        output_slice_folder = filepath + "Slices" + "\\" + j + "\\"
+        print output_slice_folder
         current = 0
         increment = 30
         text.append("mkdir " + output_slice_folder + "\n \n")
@@ -28,9 +27,9 @@ def write_slice_bat_file():
             current_time = time.strftime("%H:%M:%S", time.gmtime(current))
             print current_time
 
-            output_file = j + "_Both_Slice_{0}.mp4".format(i)  ## Change to .wmv if necessary
+            output_file = j + "_Slice_{0}.mp4".format(i)  ## Change to .wmv if necessary
             print output_file
-            text.append("ffmpeg -i " + input_batch_folder + input_file + " -ss {0} -t 00:00:30 -strict -2 ".format(current_time) + output_slice_folder + output_file + "\n \n")
+            text.append("ffmpeg -i " + filepath + input_file + " -ss {0} -t 00:00:30 -strict -2 ".format(current_time) + output_slice_folder + output_file + "\n \n")
     output = "".join(text)
     print output
     batch_output = open(filename, "w")
